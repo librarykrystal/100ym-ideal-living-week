9cb9f623756e89ce134434b05036055c16e8604f
Krys Frost <librarykrystal@gmail.com>
2023-03-30 18:46:06 -0500

    Updated database.sql.

--- database.sql
+++ database.sql
@@ -3,8 +3,44 @@
 -- You must use double quotes in every query that user is in:
 -- ex. SELECT * FROM "user";
 -- Otherwise you will have errors!
+
 CREATE TABLE "user" (
     "id" SERIAL PRIMARY KEY,
     "username" VARCHAR (80) UNIQUE NOT NULL,
-    "password" VARCHAR (1000) NOT NULL
+    "password" VARCHAR (1000) NOT NULL,
+    "email" VARCHAR (1000) NOT NULL,
+    "admin" BOOLEAN DEFAULT false
+);
+
+CREATE TABLE "question" (
+    "id" SERIAL PRIMARY KEY,
+    "question" VARCHAR (1000)
+);
+
+CREATE TABLE "answer" (
+    "id" SERIAL PRIMARY KEY,
+    "user_id" INT REFERENCES "user",
+    "question_id" INT REFERENCES "question",
+    "response" VARCHAR (1000)
+);
+
+CREATE TABLE "category" (
+    "id" SERIAL PRIMARY KEY,
+    "name" VARCHAR (100)
+);
+
+CREATE TABLE "priority" (
+    "id" SERIAL PRIMARY KEY,
+    "user_id" INT REFERENCES "user",
+    "category_id" INT REFERENCES "category",
+    "rank" INT
+);
+
+CREATE TABLE "ideal_week" (
+    "id" SERIAL PRIMARY KEY,
+    "user_id" INT REFERENCES "user",
+    "day" VARCHAR (20),
+    "start_time" time (6),
+    "end_time" time (6),
+    "category_id" INT REFERENCES "category"
 );
