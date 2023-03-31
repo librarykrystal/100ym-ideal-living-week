8901b1bd2d111f449e338e18d6601d23439cdca0
David Trelles <davidstrelles@gmail.com>
2023-03-31 16:23:39 -0500

    new code comments

--- src/components/WeekPage/WeekPage.jsx
+++ src/components/WeekPage/WeekPage.jsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import React from "react";
 
+//still working out logic on this page
 
+//Routes needed - POST to ideal_week
 function WeekPage() {
-
   return (
     <div className="container">
       <div>
