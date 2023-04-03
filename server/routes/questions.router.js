46433573110d6bb8e345fbd2ba6afade37cbe7bc
zak abdulahi <zaklahi0333@gmail.com>
2023-04-02 19:29:56 -0500

    fixed spelling error

--- server/routes/questions.router.js
+++ server/routes/questions.router.js
@@ -1,6 +1,6 @@
 router.get('/', (req, res) => {
 
-    const queryText = `SELECT * FROM "question" WHERE "category_id"=$1`;;
+    const queryText = `SELECT * FROM "question" WHERE "category_id"=$1;`;
     pool.query(queryText ).then((result) => {
       console.log(result.rows);
       res.send(result.rows);
