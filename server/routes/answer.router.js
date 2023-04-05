a1a71643219a73add702f33043544d133c9f1c07
zak abdulahi <zaklahi0333@gmail.com>
2023-04-04 20:52:47 -0500

    pushing up updated db and post in idealWeek

--- server/routes/answer.router.js
+++ server/routes/answer.router.js
@@ -16,25 +16,25 @@ router.get('/', (req, res) => {
       })
   });
   
-//// post still in works
-// router.post('/', (req, res) => {
-//     const userId = req.user.id;
-//     entry = req.body;
+// post still in works
+router.post('/', (req, res) => {
+    const userId = req.user.id;
+    entry = req.body;
     
-//     const sqlText = `INSERT INTO "answer" ("user_id", "question_id", "response") 
-//                      VALUES ($1, $2, $3)
-//                      RETURNING "id";`;
-//     const sqlParams = [entry.user_id, entry.question_id, entry.response];
+    const sqlText = `INSERT INTO "answer" ("user_id", "question_id", "response") 
+                     VALUES ($1, $2, $3)
+                     RETURNING "id";`;
+    const sqlParams = [entry.user_id, entry.question_id, entry.response];
     
-//     pool.query(sqlText, sqlParams)
-//       .then(result => {
-//         console.log(`Added answer to the database`, result.rows[0].id);
-//         res.sendStatus(201);
-//       })
-//       .catch(error => {
-//         console.log(`Error making database query ${sqlText}`, error);
-//         res.sendStatus(500);
-//       })
-//   });
-//   
+    pool.query(sqlText, sqlParams)
+      .then(result => {
+        console.log(`Added answer to the database`, result.rows[0].id);
+        res.sendStatus(201);
+      })
+      .catch(error => {
+        console.log(`Error making database query ${sqlText}`, error);
+        res.sendStatus(500);
+      })
+  });
+  
   module.exports = router;
--- server/routes/idealWeek.router.js
+++ server/routes/idealWeek.router.js
@@ -18,6 +18,28 @@ router.get('/', (req, res) => {
         res.sendStatus(500);
       })
   });
+
+  router.post('/', (req, res) => {
+    const userId = req.user.id;
+    const entry = req.body;
+  
+    const sqlText = `INSERT INTO "ideal_week" ("user_id", "day", "start_time", "end_time", "category_id") 
+                     VALUES ($1, $2, $3, $4, $5)
+                     RETURNING "id";`;
+    const sqlParams = [userId, entry.day, entry.start_time, entry.end_time, entry.category_id];
+  
+    pool.query(sqlText, sqlParams)
+      .then(result => {
+        console.log(`Added entry to the database`, result.rows[0].id);
+        res.sendStatus(201);
+      })
+      .catch(error => {
+        console.log(`Error making database query ${sqlText}`, error);
+        res.sendStatus(500);
+      })
+  });
+  
+
   
 
   module.exports = router;
