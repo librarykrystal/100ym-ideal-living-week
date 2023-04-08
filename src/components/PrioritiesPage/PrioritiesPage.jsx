d71260ce2bea9eb4d5aff497144a59f63c9911b6
Krys Frost <librarykrystal@gmail.com>
2023-04-07 19:36:05 -0500

    Cleanup/commenting, removed useEffect because fetch will happen in app.jsx.

--- src/components/PrioritiesPage/PrioritiesPage.jsx
+++ src/components/PrioritiesPage/PrioritiesPage.jsx
@@ -5,11 +5,6 @@ import { useHistory } from 'react-router-dom';
 import { DragDropContext, Droppable, Draggable } from "@hello-pangea/dnd";
 
 import Typography from '@mui/material/Typography';
-import InputLabel from '@mui/material/InputLabel';
-import MenuItem from '@mui/material/MenuItem';
-import FormControl from '@mui/material/FormControl';
-import Select from '@mui/material/Select';
-import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 
 
@@ -21,28 +16,27 @@ import Button from '@mui/material/Button';
 function PrioritiesPage() {
 
   const categories = useSelector(store => store.categories);
+    // Dispatch to GET the list of unordered categories happens in app.jsx
   const history = useHistory();
   const dispatch = useDispatch();
 
-  // Dispatch to GET the list of unordered categories happens in app.jsx
-
-  // React state to track order of items
+  // Local state to track order of items
   const [itemList, setItemList] = useState(categories);
 
   // Function to update list on drop
   const handleDrop = (droppedItem) => {
-    // Ignore drop outside droppable container
+    // Ignore if dropedp outside droppable container
     if (!droppedItem.destination) return;
     var updatedList = [...itemList];
     // Remove dragged item
     const [reorderedItem] = updatedList.splice(droppedItem.source.index, 1);
     // Add dropped item
     updatedList.splice(droppedItem.destination.index, 0, reorderedItem);
-    // Update State
+    // Update state array
     setItemList(updatedList);
   };
 
-  // bundling of ranked user input:
+  // bundling of ranked user input for dispatch:
   const rankedList = [
     {rank: 1, category_id: parseInt(`${itemList[0].id}`)},
     {rank: 2, category_id: parseInt(`${itemList[1].id}`)},
@@ -56,7 +50,7 @@ function PrioritiesPage() {
     {rank: 10, category_id: parseInt(`${itemList[9].id}`)}
   ]
 
-  console.log('RANKED:', rankedList);
+  // console.log('RANKED:', rankedList);
 
   // Handles SAVE - - - submits ranked priorities to database
   const saveAnswers = () => {
@@ -67,55 +61,54 @@ function PrioritiesPage() {
       });
   }
 
-
   return (
     <div>
       <center>
         <Typography variant="h4" mt={0} mb={1} gutterBottom>PRIORITIES PAGE</Typography>
         <Typography variant="body1" mb={6} gutterBottom>Drag and drop the categories below to prioritize them, highest priority to lowest.</Typography>
         
-      <div className="priority-container">
-        <DragDropContext onDragEnd={handleDrop}>
-          <Droppable droppableId="list-container">
-            {(provided) => (
-              <div
-                className="list-container"
-                {...provided.droppableProps}
-                ref={provided.innerRef}
-              >
-                {itemList.map((item, index) => (
-                  <Draggable key={item.name} draggableId={item.name} index={index}>
-                    {(provided) => (
-                      <div
-                        className="item-container"
-                        ref={provided.innerRef}
-                        {...provided.dragHandleProps}
-                        {...provided.draggableProps}
-                      >
-                        {item.name}
-                      </div>
-                    )}
-                  </Draggable>
-                ))}
-                {provided.placeholder}
-              </div>
-            )}
-          </Droppable>
-        </DragDropContext>
-      </div>
+        <div className="priority-container">
+          <DragDropContext onDragEnd={handleDrop}>
+            <Droppable droppableId="list-container">
+              {(provided) => (
+                <div
+                  className="list-container"
+                  {...provided.droppableProps}
+                  ref={provided.innerRef}
+                >
+                  {itemList.map((item, index) => (
+                    <Draggable key={item.name} draggableId={item.name} index={index}>
+                      {(provided) => (
+                        <div
+                          className="item-container"
+                          ref={provided.innerRef}
+                          {...provided.dragHandleProps}
+                          {...provided.draggableProps}
+                        >
+                          {item.name}
+                        </div>
+                      )}
+                    </Draggable>
+                  ))}
+                  {provided.placeholder}
+                </div>
+              )}
+            </Droppable>
+          </DragDropContext>
+        </div>
+        <br/><br/>
+
+        {/* SAVE button */}
+        <Button
+          type="submit"
+          variant="contained"
+          color="primary"
+          size="large"
+          onClick={saveAnswers}>SAVE PRIORITIES
+        </Button>
       <br/><br/>
 
-      {/* SAVE button */}
-      <Button
-      type="submit"
-      variant="contained"
-      color="primary"
-      size="large"
-      onClick={saveAnswers}>SAVE PRIORITIES
-    </Button>
-    <br/><br/>
     </center>
-
   </div>
   );
 }
