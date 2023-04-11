6bba3eb7d32ddec2e9712d138e93c4304804227e
Krys Frost <librarykrystal@gmail.com>
2023-04-11 00:18:56 -0500

    Set up AdminPage component with MUI theme, added userlist.router to get all users' info.

--- src/components/AdminPage/AdminPage.jsx
+++ src/components/AdminPage/AdminPage.jsx
@@ -1,14 +1,43 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useHistory } from 'react-router-dom';
+
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import Typography from '@mui/material/Typography';
+import "@fontsource/roboto-slab";
+import Button from '@mui/material/Button';
+
+// Material UI Font Theming
+const theme = createTheme({
+  typography: {
+    fontFamily: [
+      'Roboto Slab',
+    ],
+  },
+  palette: {
+    primary: {
+      main: '#475473',
+    },
+    secondary: {
+      main: '#1c4bd9',
+    },
+    info: {
+      main: '#bdbfbf',
+    },
+  },
+});
 
 
 function AdminPage() {
 
   return (
+    <ThemeProvider theme={theme}>
     <div className="container">
       <div>
-        <p>ADMIN PAGE</p>
+        <Typography variant="h4" mt={0} mb={1} gutterBottom>ADMIN PAGE</Typography>
       </div>
     </div>
+    </ThemeProvider>
   );
 }
 
