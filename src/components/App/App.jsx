d7881143b7722f046ddfd43b4794255a0af64765
Krys Frost <librarykrystal@gmail.com>
2023-04-13 01:06:40 -0500

    Added introComplete conditional route protection to /questions and /priorities.

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -78,13 +78,21 @@ function App() {
           </ProtectedRoute>
 
           <ProtectedRoute exact path="/questions">
-            {/* logged in shows QuestionsPage else shows LoginPage */}
-            <QuestionsPage />
+            {/* logged in shows QuestionsPage (OR IntroPage) else shows LoginPage */}
+            {!user.introComplete ?
+              <Redirect to="/intro" />  // If introComplete is falsy, redirect to IntroPage
+              :
+              <QuestionsPage />  // else, go to QuestionsPage
+            }
           </ProtectedRoute>
 
           <ProtectedRoute exact path="/priorities">
-            {/* logged in shows PrioritiesPage else shows LoginPage */}
-            <PrioritiesPage />
+            {/* logged in shows PrioritiesPage (OR IntroPage) else shows LoginPage */}
+            {!user.introComplete ?
+              <Redirect to="/intro" />  // If introComplete is falsy, redirect to IntroPage
+              :
+              <PrioritiesPage />  // else, go to PrioritiesPage
+            }
           </ProtectedRoute>
 
           <ProtectedRoute exact path="/admin">
