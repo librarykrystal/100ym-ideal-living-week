1817ef4e49000968b70be6bd935515206948b5d3
Krys Frost <librarykrystal@gmail.com>
2023-04-14 23:04:16 -0500

    RegisterPage is MUI.

--- src/components/RegisterPage/RegisterPage.jsx
+++ src/components/RegisterPage/RegisterPage.jsx
@@ -3,25 +3,46 @@ import React from 'react';
 import { useHistory } from 'react-router-dom';
 import RegisterForm from '../RegisterForm/RegisterForm';
 
+// MUI 
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import Typography from '@mui/material/Typography';
+import "@fontsource/roboto-slab";
+import ButtonBase from '@mui/material/ButtonBase';
+
+// Material UI Font Theming
+const theme = createTheme({
+  typography: {
+    fontFamily: [
+      'Roboto Slab',
+    ],
+  },
+  palette: {
+    primary: {
+      main: '#475473',
+    },
+    secondary: {
+      main: '#1c4bd9',
+    },
+  },
+});
+
 function RegisterPage() {
   const history = useHistory();
 
   return (
+    <ThemeProvider theme={theme}>
     <div>
       <RegisterForm />
-
       <center>
-        <button
-          type="button"
+        <ButtonBase
           className="btn btn_asLink"
-          onClick={() => {
-            history.push('/login');
-          }}
+          onClick={() => {history.push('/login')}}
         >
-          Login
-        </button>
+          <Typography>Login</Typography>
+        </ButtonBase>
       </center>
     </div>
+    </ThemeProvider>
   );
 }
 
