246a90ec61670470adf95a81e69d2bf914519d30
Ashlen Goff <ashlensage@icloud.com>
2023-04-16 15:20:03 -0500

    deleted dummy data

--- src/components/WeekTable/WeekTable.jsx
+++ src/components/WeekTable/WeekTable.jsx
@@ -8,6 +8,7 @@ import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
+import prioritiesSaga from "../../redux/sagas/priorities.saga";
 
 function createData(
   Categories,
@@ -33,23 +34,6 @@ function createData(
   };
 }
 
-const rows = [
-  createData("Sleep", 8.0, 7.5, 8.5, 9.0, 8.0, 9.5, 9.0, 59.5),
-  createData("Wellness", 1.0, 2.0, 3.5, 4.5, 2.5, 3.5, 2.5, 19.5),
-  createData("Outdoor Time", 4.0, 2.5, 3.0, 1.5, 2.0, 2.5, 3.0, 18.5),
-  createData("Family Time", 3.0, 3.5, 4.0, 4.5, 5.0, 4.0, 5.0, 29),
-  createData("Eating", 2.0, 3.0, 3.5, 2.5, 3.5, 3.0, 3.5, 21),
-];
-
-const daysOfWeek = [
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-  "Sunday",
-];
 
 export default function BasicTable() {
   const dispatch = useDispatch();
