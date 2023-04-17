c950c7bdbd2ffa8057724a0147b4daade3526f76
David Trelles <davidstrelles@gmail.com>
2023-04-17 10:12:22 -0500

    footer now updated to manifesto footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,13 +1,24 @@
-import React from 'react';
-import './Footer.css';
-
+import React from "react";
+import "./Footer.css";
+import { Typography, Link } from "@mui/material";
 // This is one of our simplest components
 // It doesn't have local state, so it can be a function component.
 // It doesn't dispatch any redux actions or display any part of redux state
 // or even care what the redux state is, so it doesn't need 'connect()'
 
 function Footer() {
-  return <footer>&copy; Prime Digital Academy</footer>;
+  return (
+    <footer>
+      <Typography variant="body2" color="textSecondary" align="center">
+        {" © "}
+        <Link color="inherit" href="https://www.100yearmanifesto.com/">
+          100 Year Manifesto
+        </Link>{" "}
+        {new Date().getFullYear()}
+        {"."}
+      </Typography>
+    </footer>
+  );
 }
 
 export default Footer;
