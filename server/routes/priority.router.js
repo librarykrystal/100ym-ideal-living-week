a9f7fd1b86312bc0882a0c99801530a813b2f91f
zak abdulahi <zaklahi0333@gmail.com>
2023-04-17 18:05:43 -0500

    put router functioning, new user still needs to be fixed

--- server/routes/priority.router.js
+++ server/routes/priority.router.js
@@ -3,16 +3,30 @@ const pool = require('../modules/pool');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-
-  const queryText = `SELECT * FROM "priority" ORDER BY RANK;`;
-    pool.query(queryText ).then((result) => {
-      console.log(result.rows);
+  const queryText = `
+    SELECT "category".* FROM "priority" JOIN "category" ON "priority"."category_id" = "category"."id" 
+    WHERE "user_id" = $1 ORDER BY "rank";
+  `;
+  pool.query(queryText, [req.user.id] ).then((result) => {
+    console.log(result.rows);
+    if(result.rows === 0) {
+      // No results found, return category list
+      const queryText2 = 'SELECT * FROM "category" ORDER BY "id";';
+      pool.query(queryText2 ).then((result) => {
+        console.log(result.rows);
+        res.send(result.rows);
+      }).catch((err) => {
+        console.log('err w get request', err);
+        res.sendStatus(500);
+      });
+    } else {
       res.send(result.rows);
-    }).catch((err) => {
-      console.log('err w get request', err);
-      res.sendStatus(500);
-    });
+    }
+  }).catch((err) => {
+    console.log('err w get request', err);
+    res.sendStatus(500);
   });
+});
 
 //   router.post('/', (req, res) => {
 //     const user_id = req.body.user_id;
@@ -35,10 +49,16 @@ router.get('/', (req, res) => {
 router.put('/', (req, res) => {
   console.log('this is req.body from router', req.body)
     const entry = req.body;
-    const user = req.user.id;
-    const sqlParams = [ entry.category_id, entry.rank, entry.user_id]
-    const sqlText = `UPDATE "priority" SET "category_id" = $1
-    WHERE "rank" = $2 AND "user_id" = $3;`;
+    const sqlParams = [ entry.rank, entry.category_id, req.user.id]
+    const sqlText = `
+      -- First try to do an insert
+      INSERT INTO "priority" ("rank", "category_id", "user_id")
+      VALUES ($1, $2, $3)
+      -- Check for existing values
+      ON CONFLICT("category_id", "user_id")
+      -- If existing values, update instead
+      DO UPDATE SET "rank" = $1;
+    `;
     pool.query(sqlText, sqlParams )
         .then((result) => {
             res.sendStatus(200);
