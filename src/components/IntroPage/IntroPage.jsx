b02b5212dfad2233fcbe165cd651a79eed5c36f5
Krys Frost <librarykrystal@gmail.com>
2023-04-18 01:57:37 -0500

    IntroPage button now has history.push to /questions.

--- src/components/IntroPage/IntroPage.jsx
+++ src/components/IntroPage/IntroPage.jsx
@@ -28,43 +28,46 @@ const theme = createTheme({
 });
 
 function IntroPage() {
-  const dispatch = useDispatch();
-const answers = {
-  1: '',
-  2: '',
-  3: '',
-  4: '',
-  5: '',
-  6: '',
-  7: '',
-  8: '',
-  9: '',
-  10: '',
-  11: '',
-  12: '',
-  13: '',
-  14: '',
-  15: '',
-  16: '',
-  17: '',
-  18: '',
-  19: '',
-  20: '',
-  21: '',
-  22: '',
-  23: '',
-  24: '',
-  25: '',
-  26: '',
-  27: '',
-  28: '',
-  29: '',
-  30: ''
 
-}
-const postAndContinue = () => {
-  console.log('SAVE & CONTINUE clicked');
-  dispatch({
+  const dispatch = useDispatch();
+  const history = useHistory();
+
+  const answers = {
+    1: '',
+    2: '',
+    3: '',
+    4: '',
+    5: '',
+    6: '',
+    7: '',
+    8: '',
+    9: '',
+    10: '',
+    11: '',
+    12: '',
+    13: '',
+    14: '',
+    15: '',
+    16: '',
+    17: '',
+    18: '',
+    19: '',
+    20: '',
+    21: '',
+    22: '',
+    23: '',
+    24: '',
+    25: '',
+    26: '',
+    27: '',
+    28: '',
+    29: '',
+    30: ''
+  }
+
+  const postAndContinue = () => {
+    console.log('SAVE & CONTINUE clicked');
+    dispatch({
       type: 'POST_ANSWERS',
       payload: answers
     });
@@ -72,7 +75,8 @@ const postAndContinue = () => {
     dispatch({
       type: 'INTRO_PAGE_DONE'
     });
-}
+    history.push(`/questions`);
+  }
 
   return (
     <ThemeProvider theme={theme}>
