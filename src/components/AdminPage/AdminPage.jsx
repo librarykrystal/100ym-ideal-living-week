24450a734247eb7bea8bf306eacff27301b84f25
Krys Frost <librarykrystal@gmail.com>
2023-04-22 20:52:10 -0500

    Added EDIT QUESTIONS button on AdminPage, renders only if user is admin.

--- src/components/AdminPage/AdminPage.jsx
+++ src/components/AdminPage/AdminPage.jsx
@@ -1,10 +1,12 @@
 import React, { useEffect, useLayoutEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { useHistory } from "react-router-dom";
 
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import Typography from '@mui/material/Typography';
 import "@fontsource/roboto-slab";
 import Box from '@mui/material/Box';
+import Button from "@mui/material/Button";
 
 // Material UI Font Theming
 const theme = createTheme({
@@ -29,9 +31,11 @@ const theme = createTheme({
 
 function AdminPage() {
 
-  const dispatch = useDispatch();
+  const user = useSelector((store) => store.user);
   const userList = useSelector(store => store.userList);
-
+  const dispatch = useDispatch();
+  const history = useHistory();
+  
   // Dispatch (on page load) to GET all the users
   useEffect(() => {
     dispatch({ type: 'FETCH_USERLIST' });
@@ -42,13 +46,34 @@ function AdminPage() {
     window.scrollTo(0, 0)
   }, []);
 
+    // Handles EDIT QUESTIONS button click
+    const goEditQuestions = () => {
+      console.log("EDIT QUESTIONS clicked");
+      history.push(`/editquestions`);
+    };
+
   return (
     <ThemeProvider theme={theme}>
     <div className="container">
       <div>
         <Typography variant="h4" mt={0} mb={5} gutterBottom>ADMIN PAGE</Typography>
       </div>
-      <Typography variant="h5" mt={0} mb={1} gutterBottom>USERS</Typography>
+      {user.admin &&
+      <>
+        <Button
+        variant="contained"
+        color="primary"
+        size="large"
+        onClick={goEditQuestions}
+        >
+        EDIT QUESTIONS
+        </Button>
+        <br/>
+        <br/>
+        </>
+      }
+
+      <Typography variant="h5" mt={2} mb={1} gutterBottom>USERS</Typography>
       <Box>
         {/* Conditional to avoid rendering before users array arrives from Redux store */}
         {userList.length > 0 &&
